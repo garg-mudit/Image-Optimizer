1210f6bc2dc97a57b990c9aad80714ba711048a4
Mudit Garg <itzmuditgarg@gmail.com>
2020-09-04 23:45:50 +0530

    Updated Menu Items, Added New About Window

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow, Menu, globalShortcut } = require('electron');
-const { menu } = require('./app/menu');
+const { app, BrowserWindow, Menu } = require('electron');
+// const { menu } = require('./app/menu');
 const { isDev, isMac } = require('./app/envVariables');
 
 let mainWindow;
+let aboutWindow;
 
 function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -18,15 +19,66 @@ function createMainWindow() {
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
 
-  //register custom global shortcuts
-  globalShortcut.register('CmdOrCtrl+R', () => mainWindow.reload());
-  globalShortcut.register(isMac ? 'Command+Alt+I' : 'Ctrl+Shift+I', () =>
-    mainWindow.toggleDevTools()
-  );
-
   //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
   mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
+
+function createAboutWindow() {
+  aboutWindow = new BrowserWindow({
+    title: 'About ImageShrink',
+    width: 300,
+    height: 300,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: false,
+    backgroundColor: '#ffffff',
+  });
+  aboutWindow.setMenu(null);
+  aboutWindow.loadFile(`${__dirname}/app/about.html`);
+}
+
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [{ label: 'About', click: createAboutWindow }],
+        },
+      ]
+    : []),
+  { role: 'fileMenu' },
+  ...(isMac
+    ? []
+    : [
+        {
+          label: 'Help',
+          submenu: [
+            {
+              label: 'About',
+              click: () => {
+                if (aboutWindow != null) {
+                  aboutWindow.close();
+                  aboutWindow = null;
+                }
+                createAboutWindow();
+              },
+            },
+          ],
+        },
+      ]),
+  ...(isDev
+    ? [
+        {
+          label: 'Developer',
+          submenu: [
+            { role: 'reload' },
+            { role: 'forcereload' },
+            { type: 'separator' },
+            { role: 'toggledevtools' },
+          ],
+        },
+      ]
+    : []),
+];
 
 app.on('ready', () => {
   createMainWindow();
