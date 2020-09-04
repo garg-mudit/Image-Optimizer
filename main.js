ce649370ff93d76469fdb73aab93a1fd8a1e691c
Mudit Garg <itzmuditgarg@gmail.com>
2020-09-04 10:45:31 +0530

    Added basic index file and added assets for app icon logo.

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 const { app, BrowserWindow } = require('electron');
+
+//Set env
+process.env.NODE_ENV = 'development';
+
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+
+//PlatformInto (Mac -> Darwin, Windows -> win32, linux -> linux)
+const isMac = process.platform === 'darwin' ? true : false;
+
 let mainWindow;
+
 function createMainWindow() {
   mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
   });
 
-  mainWindow.loadURL('https://dev.vault-erp.com');
+  //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
 app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows.length === 0) {
+    createMainWindow();
+  }
+});
